1ecf11dc9dc43686bcef131775f6b4721e8f7b6d
qq516333132 <516333132@qq.com>
2017-04-17 19:37:31 +0800

    可以显示 24bit,RGB888 / 16bit,RGB565/ 8bit灰度 / 1bit 单色显示， 赶脚8bit灰度显示有bug

--- uCGUI398/Config/LCDConf.h
+++ uCGUI398/Config/LCDConf.h
@@ -8,10 +8,10 @@
 **********************************************************************
 */
 
-#define LCD_XSIZE              (480)    //配置LCD的水平分辨率
-#define LCD_YSIZE              (272)    //配置LCD的垂直分辨率
-#define LCD_BITSPERPIXEL  (24)     //每个像素的位数
-#define LCD_FIXEDPALETTE (888)    //调色板格式
+#define LCD_XSIZE              (800)    //配置LCD的水平分辨率
+#define LCD_YSIZE              (480)    //配置LCD的垂直分辨率
+#define LCD_BITSPERPIXEL		(24)     //每个像素的位数
+#define LCD_FIXEDPALETTE		(888)    //调色板格式
 #define LCD_SWAP_RB         (1)      //红蓝反色交换
 #define LCD_CONTROLLER  (123)  //LCD控制器的名称
 
